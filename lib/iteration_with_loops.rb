f2029fa5e62636feb91aad4a656e79ac9d72ec7b
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-23 21:57:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   outer_results = []
   row_index = 0
   binding.pry
-  while row_index < src[row_index].length do
+  while row_index < src[row_index].length
     outer_results << src[row_index].min
     row_index += 1
   end
